a086b742c8fcbd569e8a6be5e6798f19181f45ef
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-29 22:33:25 -0700

    Initial Commit - Base firebase setup
    firebase unique link needed in the controller

---
+++ FirebaseController.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseController.swift
+//  Firebase_Test
+//
+//  Created by Diego Aguirre on 2/29/16.
+//  Copyright © 2016 Diego Aguirre. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseController {
+    
+    static let base = Firebase(url: "")
+}
